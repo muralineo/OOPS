404cdebbc6a95eb7462ceed38a41db94db9d38e4
murali4aji <muralim@applieddatafinance.com>
2022-02-02 22:14:31 +0530

    helper.py

---
+++ basic/helper.py
@@ -0,0 +1,23 @@
+# When to use class methods and when to use static methods?
+
+class Item:
+    @staticmethod
+    def is_integer():
+        """
+        This should do something that has a relationship with the class,
+        but not something that must be unique per instance
+        """
+
+    @classmethod
+    def instantiate_from_something(cls):
+        """
+        This should also do something that has a relationship with the class, but usually,
+        those are used to manipulate different structures of data to instantiate objects
+        like we have done with CSV
+        """
+
+
+# These methods could also be called from instances
+item1 = Item()
+item1.is_integer()
+item1.instantiate_from_something()
--- basic/m1.py
+++ basic/m1.py
@@ -33,6 +33,18 @@ class Item:
                 quantity=int(item.get('quantity')),
             )
 
+    @staticmethod
+    def is_integer(num):
+        # We will count the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
@@ -44,6 +56,9 @@ class Item:
         return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
 
 
-Item.instantiate_from_csv()
-print(Item.all)
+# Item.instantiate_from_csv()
+# print(Item.all)
+# print(Item.is_integer(7.5))
+
+
 
